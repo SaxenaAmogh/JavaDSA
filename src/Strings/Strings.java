98bfcf2efc806ecc60aa3690deaf3a9431b59d4c
Amogh Saxena <amoghsaxena2004@gmail.com>
2026-01-05 23:32:35 +0530

    Stings file

---
+++ src/Strings/Strings.java
@@ -0,0 +1,13 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class Strings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = "Amogh ";
+        String surname = sc.nextLine();
+
+        System.out.println(name + surname);
+    }
+}
