326f805b3ae4ba2c8f68a81a5b39f33b0334f4e2
Amogh Saxena <amoghsaxena2004@gmail.com>
2026-01-26 10:11:03 +0530

    Optimized Bubble Sort

--- src/Sorting/bubbleSort.java
+++ src/Sorting/bubbleSort.java
@@ -6,12 +6,17 @@ public class bubbleSort {
         int len = array.length;
 
         for (int i = len - 1; i>=1; i--){
+            int swapCount = 0;
             for (int j = 0; j<=i-1; j++){
                 if (array[j]>array[j+1]){
                     int temp = array[j];
                     array[j] = array[j+1];
                     array[j+1] = temp;
+                    swapCount++;
                 }
+            }
+            if (swapCount == 0){
+                break;
             }
         }
         for (int k : array) {
